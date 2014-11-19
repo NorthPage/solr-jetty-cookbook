ced77363637f3f93209162365fe5ffff69d779b8
E. Camden Fisher <fish@fishnix.net>
2014-11-19 14:20:41 -0500

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             'solr-jetty'
+maintainer       'E Camden Fisher'
+maintainer_email 'camden@northpage.com'
+license          'Apache 2.0'
+description      'Installs/Configures solr-jetty'
+long_description 'Installs/Configures solr-jetty'
+version          '0.1.2'
+
+depends 'libarchive', '~> 0.4.1'
+depends 'java', '~> 1.0'
+depends 'iptables', '~> 0.14.0'
---
+++ spec/unit/recipes/prep_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+
+describe 'solr-jetty::prep' do
+
+  platforms = {
+      'centos' => ['6.5'],
+      'ubuntu' => ['12.04', '14.04']
+  }
+
+  platforms.each do |platform, versions|
+    versions.each do |version|
+
+      context "on #{platform.capitalize} #{version}" do
+        let (:chef_run) do
+          ChefSpec::Runner.new(log_level: :warn, platform: platform, version: version) do |node|
+            # set additional node attributes here
+          end.converge(described_recipe)
+        end
+
+        it 'should include java::default by default' do
+          expect(chef_run).to include_recipe('java::default')
+        end
+
+        it 'should create the solr user' do
+          expect(chef_run).to create_user('solr')
+        end
+      end
+    end
+  end
+end
