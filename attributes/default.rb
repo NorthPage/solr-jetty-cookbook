2b32ccd6d9b18add6929de5e47963af90b775008
E. Camden Fisher <fish@fishnix.net>
2015-07-01 10:17:19 -0400

    Add support for CentOS 7 and systemd

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,10 @@ default['solr-jetty']['port']     = '8983'
 default['solr-jetty']['user']     = 'solr'
 default['solr-jetty']['install_java'] = true
 default['solr-jetty']['manage_user'] = true
+
+# determine if we use systemd or initd
+if node['platform_family'] == 'rhel' && Gem::Version.new(node['platform_version']) >= Gem::Version.new('7.0.0')
+  default['solr-jetty']['init_system'] = 'systemd'
+else
+  default['solr-jetty']['init_system'] = 'initd'
+end
